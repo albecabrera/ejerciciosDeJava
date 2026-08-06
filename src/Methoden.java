389f614c91f92cc734a36ff89193e0b22778f30e
albecabrera <albe.cabrera@yahoo.de>
2026-08-06 20:46:33 +0200

    chore: add Java methods example and source logo asset

---
+++ src/Methoden.java
@@ -0,0 +1,15 @@
+public class Methoden {
+    public static void main(String[] args) {
+    // So ruf man eine Methode in Java auf
+        ejemplometode();
+    }
+
+    public static void ejemplometode() {
+        System.out.println("Beispiel einer Methode");
+    }
+
+
+
+
+    }
+}
